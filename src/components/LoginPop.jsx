071770d6cfa0348ff38e6fb730bd0cbc081121e3
zakaria552 <zakaria.farah55@outlook.com>
2022-12-08 11:53:49 +0000

    added a login functionality and context state to check if logged in

--- src/components/LoginPop.jsx
+++ src/components/LoginPop.jsx
@@ -1,26 +1,34 @@
-import { useState, useContext } from "react"
+import { useState, useContext, useEffect } from "react"
 import "./loginPop.css"
 import {UserContext} from "../contexts/User"
+import { Login } from "../contexts/Login"
+import { getUsers } from "../api"
 function LoginPop () {
     const [username, setUsername] = useState("")
     const {user, setUser} = useContext(UserContext)
-    const loginPop = document.getElementsByClassName("login-form")
+    const {setLogin} = useContext(Login)
+    const [invalidUser, setInvalidUser] = useState(true)
+    const [validUsers, setValidUsers] = useState([])
+    
+    useEffect(() => {
+        getUsers().then((users) => {
+            setValidUsers(users)
+        })
 
+    }, [])
     const handleSubmit = (e) => {
         e.preventDefault()
-        setUser({username: username, isLoggedIn: true})
-        console.log("created account")
-        console.dir(e.target.value)
-        e.target.style.display = "none"
+        const userObj = validUsers.find((validUser) => validUser.username === username)
+        userObj ? setUser({...userObj, isLoggedIn: true}): setInvalidUser(false);
+        userObj ? setLogin(false): setLogin(true)
         setUsername("")
     }
     return (
-        <form className="login-form" onSubmit={handleSubmit}>
+        <form className={`login-form`} onSubmit={handleSubmit}>
             <p onClick={(e) => {
-                 console.dir(e.target)
-                 loginPop["0"].style.display = "none"
+                 setLogin(false)
             }}>x</p>
-            <h2>create accoutn</h2>
+            <h2>log in</h2>
             <label>
                 username
             </label>
@@ -30,6 +38,7 @@ function LoginPop () {
                 placeholder="watson" 
                 onChange={(e) => setUsername(e.target.value)}
                 />
+            <span>invalid user</span>
             <button type="submit">submit</button>
         </form>
     )
