467170ab459e9ebbc1e5e33012a3b2e3b77ab14d
zakaria552 <zakaria.farah55@outlook.com>
2022-12-09 09:11:30 +0000

    changed the getArticles function

--- src/api.js
+++ src/api.js
@@ -4,8 +4,8 @@ const ncNews = axios.create({
     baseURL: "https://nc-news-9og4.onrender.com/api"
 })
 
-export const getArticles = () => {
-    return ncNews.get("/articles")
+export const getArticles = (topic) => {
+    return ncNews.get("/articles", {params: {topic}})
         .then((res) => {
         return res.data.articles
     })
@@ -50,11 +50,5 @@ export const postComment = (user, comment_body, article_id) => {
 export const getUsers = () => {
     return ncNews.get("/users").then((res) => {
         return res.data.users
-    })
-}
-
-export const getArticlesByTopic = (topic) => {
-    return ncNews.get("/articles").then((res) => {
-        return res.data.articles.filter((article) => article.topic === topic)
     })
 }
